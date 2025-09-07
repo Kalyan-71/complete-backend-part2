5e9ef42efee831cc1d620fc37f55a3bc59011e1c
kalyan kumar <pothugantikalyankumar71@gmail.com>
2025-09-08 02:36:46 +0530

    regiter  route completed

--- src/app.js
+++ src/app.js
@@ -10,9 +10,19 @@ app.use(cors({
 }))
 
 app.use(express.json({limit: "16kb"}))
-app.use(express.urlencoded({extented:true , limit:"16kb"}))
+app.use(express.urlencoded({extended:true , limit:"16kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
 
 
+///routes import
+import userRouter from "./routes/user.routes.js"
+
+
+///routes delaration
+app.use("/api/v1/users",userRouter)
+
+
+
+///http://localhost:8000/api/v1/users/register
 export { app };
